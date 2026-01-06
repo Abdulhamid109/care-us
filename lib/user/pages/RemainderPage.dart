1f0ba3b8bb3aa7f59333a3d2f6edea3986025c6f
Abdulhamid109 <abdulhamidpatel109@gmail.com>
2026-01-06 08:18:14 +0530

    started with remainder page

---
+++ lib/user/pages/RemainderPage.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class Remainderpage extends StatefulWidget {
+  const Remainderpage({super.key});
+
+  @override
+  State<Remainderpage> createState() => _RemainderpageState();
+}
+
+class _RemainderpageState extends State<Remainderpage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text("Here we will be making the manipulations for course duration ,medicine remainder stuff.."),
+        
+      ),
+    );
+  }
+}
